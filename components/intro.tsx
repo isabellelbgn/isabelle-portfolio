e82201d24b1a1df0625f0acbe00d56aa43556c28
isabellelbgn <116334027+isabellelbgn@users.noreply.github.com>
2024-06-03 20:15:43 +0800

    feat(header): fix header styles and refactor component
    - Install clsx for conditional classNames
    - Install react-intersection-observer for scroll detection
    - Add hooks for active section context
    - Create ActiveSectionContext for managing active section state
    - Refactor header component to use ActiveSectionContext and new hooks

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,102 +1,114 @@
 "use client";
 
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link';
-import { motion } from 'framer-motion'
+import React, { useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
-import { FaGithubSquare } from 'react-icons/fa';
+import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
   return (
-    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
-        <div className='flex items-center justify-center'>
-            <div className='relative'>
-                <motion.div
-                    initial = {{ opacity: 0, scale: 0}}
-                    animate = {{ opacity: 1, scale: 1}}
-                    transition = {{
-                        type: "tween",
-                        duration: 0.2
-                    }}
-                >
-                <Image 
-                src="https://i.ibb.co/hV6nqc0/memoji.jpg" 
-                alt="memoji" 
-                width="192" 
-                height="192" 
-                quality="95" 
-                priority={true}
-                className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'/>
-                </motion.div>
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="https://i.ibb.co/hV6nqc0/memoji.jpg"
+              alt="memoji"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
 
-                <motion.span className='absolute text-4xl bottom-0 right-0'
-                    initial = {{ opacity: 0, scale: 0 }}
-                    animate = {{ opacity: 1, scale: 1 }}
-                    transition= {{
-                        type: "spring",
-                        stiffness: 125,
-                        delay: 0.5,
-                        duration: 0.7
-                    }}
-                >
-                    👋🏻 
-                </motion.span>
-            </div>
+          <motion.span
+            className="absolute text-4xl bottom-0 right-0"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.5,
+              duration: 0.7,
+            }}
+          >
+            👋🏻
+          </motion.span>
         </div>
+      </div>
 
-        <motion.h1 
-        className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
-        initial = {{ opacity: 0, y: 100 }}
-        animate = {{ opacity: 1, y: 0 }}
-        >
-            <span className='font-bold'>Hello, I'm Isabelle!</span> An aspiring 
-            <span className='font-bold'> web developer</span> focusing on 
-            <span className='font-bold'> backend development.</span> My expertise lies in 
-            <span className='font-bold'> Node.js and Django</span>, with extensive knowledge in 
-            <span className='font-bold'> React.</span>
-        </motion.h1>
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Isabelle!</span> An aspiring
+        <span className="font-bold"> web developer</span> focusing on
+        <span className="font-bold"> backend development.</span> My expertise
+        lies in
+        <span className="font-bold"> Node.js and Django</span>, with extensive
+        knowledge in
+        <span className="font-bold"> React.</span>
+      </motion.h1>
 
-        <motion.div 
-        className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
-        initial = {{ opacity: 0, y: 100 }}
-        animate = {{ opacity: 1, y: 0 }}
-        transition= {{
-            delay: 0.1,
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
         }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact Me
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
 
-        > 
-            <Link href="#contact"
-            className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"> 
-            Contact Me 
-            <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/> 
-            </Link>
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/CV.pdf"
+          download
+        >
+          Download Resume
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
 
-            <a 
-            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" 
-            href="/CV.pdf"
-            download> 
-            Download Resume 
-            <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/>
-            </a>
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/isabellelabuguen/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
 
-            <a 
-            className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10'
-            href="https://www.linkedin.com/in/isabellelabuguen/"
-            target="_blank"> 
-            <BsLinkedin />
-            </a>
-
-            <a 
-            className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.10rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10'
-            href="https://github.com/isabellelbgn"
-            target="_blank"> 
- 
-            <FaGithubSquare />
-            </a>
-
-        </motion.div>
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.10rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/isabellelbgn"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
-  )
+  );
 }
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
+  const { ref } = useSectionInView("Projects", 0.5);
+
   return (
-    <section>
-      <SectionHeading> My Projects </SectionHeading>
+    <section ref={ref} id="projects" className="scroll-mt-28">
+      <SectionHeading> Projects </SectionHeading>
       <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
